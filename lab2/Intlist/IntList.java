7a986170fe34cd23a7532862006e353c82cf6dd7
Zzy5201 <2721167501@qq.com>
2026-01-04 17:42:22 +0800

    123

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+        while (A.rest != null) {
+            A = A.rest;
+        }
+        A.rest = B;
+        return ptr;
     }
 
     /**
@@ -91,7 +96,15 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = new IntList();
+        IntList res = ptr;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return res.rest;
     }
 
 
